485b77f041c119b6b1dbbbb3f106f80ef2c01c14
DylanMH <dylan98909@yahoo.com>
2024-01-11 16:47:38 -0600

    more deleted files

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import LoginPage from "./components/LoginPage";
@@ -8,17 +7,12 @@ import SlidesPage from "./SlidesPage";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <Router>
-      <Routes>
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/slides" element={<SlidesPage />} />
-      </Routes>
-    </Router>
-  </React.StrictMode>
+	<React.StrictMode>
+		<Router>
+			<Routes>
+				<Route path="/" element={<LoginPage />} />
+				<Route path="/slides" element={<SlidesPage />} />
+			</Routes>
+		</Router>
+	</React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
